3448df9387d7ee3bae17e9f65b5794c12096ae91
Philip Wanczycki <p.wanczycki@gmail.com>
2022-11-18 11:01:35 -0500

    add training code skeleton

---
+++ training.py
@@ -0,0 +1,53 @@
+import env as game_env
+
+# Import os for file path management
+import os
+# Import Base Callback for saving models
+from stable_baselines3.common.callbacks import BaseCallback
+# Check Environment
+from stable_baselines3.common import env_checker
+
+from stable_baselines3 import DQN
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
+
+import cv2
+from matplotlib import pyplot as plt
+
+
+env = game_env.NGame()
+
+env_checker.check_env(env)
+
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
+
+
+CHECKPOINT_DIR = './train/'
+LOG_DIR = './logs/'
+
+callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
+
+model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=1200000, learning_starts=1000)
+
+model.learn(total_timesteps=10000, callback=callback)
+
+"""
+plt.imshow(cv2.cvtColor(env.get_observation(), cv2.COLOR_BGR2RGB))
+plt.show()
+"""
